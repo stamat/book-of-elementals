4fd45cdb62d529d25d87c07f8b667aec1f7fed09
Nikola Stamatovic <stamatmail@gmail.com>
2026-09-06 01:20:16 +0200

    Build version 3.4.1

--- dist/elementals/rearrange.js
+++ dist/elementals/rearrange.js
@@ -1,4 +1,4 @@
-/* book-of-elementals v3.4.0 | https://stamat.github.io/book-of-elementals/ | MIT License */
+/* book-of-elementals v3.4.1 | https://stamat.github.io/book-of-elementals/ | MIT License */
 (() => {
   // node_modules/book-of-spells/src/helpers.mjs
   function shallowMerge(target, source) {
